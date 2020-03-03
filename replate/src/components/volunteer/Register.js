c4d09c444a925122a6b2a1a4dbc76b74b631e8ec
Easton haaff <eastonahaaff1998@gmail.com>
2020-03-02 18:02:06 -0600

    Added some basic validation

--- replate/src/components/volunteer/Register.js
+++ replate/src/components/volunteer/Register.js
@@ -1,24 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
 import "./register.css"
 
 const VolunteerRegister = () => {
+    const [pwd1, setPwd1] = useState(); //first pw input
+    const [pwd2, setPwd2] = useState(); //confirmed password input
 
     const handleSubmit = event =>{
         event.preventDefault();
+        if(pwd1 === pwd2){
+            //API work here?
+            
+        } else{
+            alert("Your passwords do not match")
+        }
     }
     return (
         <div className="registerContainer">
             <form onSubmit={handleSubmit}>
                 <label htmlFor="name">First and Last name:</label>
-                <input id="name" id="name"></input>
+                <input id="name" id="name" required></input>
+
                 <label htmlFor="phone">Phone Number</label>
-                <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
-                <label htmlFor="username">Username:</label>
-                <input id="username" id="username"></input>
-                <label htmlFor="password">Password:</label>
-                <input type="password"></input>
+                <input type="tel" id="phone" name="phone" required></input>
+
+                <label htmlFor="username" >Username:</label>
+                <input id="username" required></input>
+
+                <label htmlFor="password" required>Password:</label>
+                <input type="password" minlength='8' onChange={event => setPwd1(event.target.value)} required></input>
+
                 <label htmlFor="password">Confirm Password:</label>
-                <input type="password" id="confirmPassword"></input>
+                <input type="password" id="confirmPassword" onChange={event => setPwd2(event.target.value)} required></input>
+
                 <button type="submit">Submit</button>
             </form>
         </div>
