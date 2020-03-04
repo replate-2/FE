a3800c8d06fe3597f4919b916723894645f617e5
unknown <william.z.wilson95@gmail.com>
2020-03-04 03:00:41 -0600

    fixed minor errors

--- replate/src/components/general/Dashboard.js
+++ replate/src/components/general/Dashboard.js
@@ -9,15 +9,16 @@ const Dashboard = () =>{
                 <Navigation />
                 <h2>Are you a Business or a Volunteer?</h2>
                 <div className="buttonContainer">
-      
+
                     <div className="buisnessContainer">
                         <button><Link to="/Business/SignIn">Business</Link></button>
                     </div>
-      
+
                     <div className="volunteerContainer">
                     <button><Link to="/volunteer/login">Volunteer</Link></button>
-                 </div>
-      
+                    </div>
+                    
+                </div>
             </div>
     )
 }
