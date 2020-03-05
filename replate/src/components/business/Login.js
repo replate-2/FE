a0479bd57ee3064a23c6fbeb213106a264edd75d
unknown <william.z.wilson95@gmail.com>
2020-03-05 17:43:29 -0600

    stylesssss

--- replate/src/components/business/Login.js
+++ replate/src/components/business/Login.js
@@ -1,75 +1,82 @@
 import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom";
 import * as yup from "yup";
 import styled from "styled-components";
-import { axiosWithAuth } from "../../utils/axiosWithAuth"
-
-
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
 const Beef = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-
 const SignInForm = () => {
   const [signInBiz, setSignInBiz] = useState({
     username: "",
-    password: "",
-  })
+    password: ""
+  });
 
-const history = useHistory();
+  const history = useHistory();
 
-const handleChange = e => {
-  e.preventDefault();
-  setSignInBiz({
-      ...signInBiz, [e.target.name]: e.target.value
-  })
-}
+  const handleChange = e => {
+    e.preventDefault();
+    setSignInBiz({
+      ...signInBiz,
+      [e.target.name]: e.target.value
+    });
+  };
 
-const handleSubmit = e => {
-  e.preventDefault();
-  axiosWithAuth()
-    .post("api/auth/business/login", signInBiz)
-    .then(res => {
-      console.log(res)
-      window.localStorage.setItem('token', res.data.payload)
-      history.push("/Business/Profile")
-    })
-    .catch(err => {
-      console.log(err)
-    })
-}
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("api/auth/business/login", signInBiz)
+      .then(res => {
+        console.log(res);
+        window.localStorage.setItem("token", res.data.payload);
+        history.push("/Business/Profile");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div>
-      <div>
+    <div className="cont">
+      <Beef>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
-          <input 
-          type="text"
-          id="username"
-          name="username"
-          value={signInBiz.username}
-          onChange={handleChange}
-          required
-          />
+          <div>
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              name="username"
+              value={signInBiz.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-          <label htmlFor="password">Password</label>
-          <input 
-          type="password"
-          id="password"
-          name="password"
-          value={signInBiz.password}
-          onChange={handleChange}
-          required
-          />
+          <div className="beef2">
+            <input
+              type="password"
+              placeholder="password"
+              id="password"
+              name="password"
+              value={signInBiz.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-          <button type="submit">Submit</button>
+          <button className='formbutton' type="submit">Submit</button>
+
+          <div className='sb'>
+          <span>Dont have an account yet? <Link className='link' to="/Business/Signup">Sign up here!</Link></span>
+          </div>
+
         </form>
-      </div>
+      </Beef>
     </div>
-  )
-}
+  );
+};
 
-export default SignInForm
+export default SignInForm;
