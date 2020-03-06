430978d7907f767f286ef89980d51c5377019154
William Ryan <billy.ryan.rom@gmail.com>
2020-03-06 03:47:52 -0500

    Add Link to ProfileEdit business in Profile.js business

--- replate/src/components/business/Profile.js
+++ replate/src/components/business/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 import { connect } from "react-redux"
+import { Link } from "react-router-dom"
 
 
 import { fetchBusiness } from "../../actions"
@@ -10,12 +11,12 @@ const BusinessProfile = props => {
     useEffect(() => {
         props.fetchBusiness()
     }, [])
-    console.log(props)
+    
     return <div>
-        <h1>Welcome</h1>
-        <p>{props.business.businessName}</p>
-        <p>{props.business.businessAddress}</p>
-        <p>{props.business.businessPhone}</p>
+        <h1>Welcome {props.business.businessName}</h1>
+        <h2>Your Address: {props.business.businessAddress}</h2>
+        <p>Your Phone Number: {props.business.businessPhone}</p>
+        <button><Link to="/Business/Profile"></Link></button>
         </div>
 }
 
