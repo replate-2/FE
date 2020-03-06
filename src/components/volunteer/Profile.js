aa0ea518ec02d3544e78c23d83cb5e39231aa784
William Ryan <billy.ryan.rom@gmail.com>
2020-03-06 13:06:58 -0500

    Add Current Food Pickup Requests to Profile.js volunteer

--- src/components/volunteer/Profile.js
+++ src/components/volunteer/Profile.js
@@ -1,13 +1,15 @@
 import React, {useEffect} from "react"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
+import FoodCard from "./FoodCard"
 
-import { fetchVolunteer } from "../../actions/volunteer"
+import { fetchVolunteer, fetchFood } from "../../actions/volunteer"
 
 const VolunteerProfile = props => {
 
     useEffect(() => {
         props.fetchVolunteer()
+        props.fetchFood()
     }, [])
     
     return <div>
@@ -15,18 +17,27 @@ const VolunteerProfile = props => {
         <h2>Your Name: {props.volunteer.name}</h2>
         <p>Your Phone Number: {props.volunteer.phoneNumber}</p>
         <button><Link to="/volunteer/edit">Edit Profile</Link></button>
+        <h1>Current Pickup Requests</h1>
+        <div className = "card-container">
+            {props.food.map(donor => (
+                <FoodCard donor={donor}/>
+            ))}
         </div>
+    </div>
 }
 
 const mapStateToProps = state => {
     return {
         volunteer: state.vol.volunteer,
         isFetching: state.vol.isFetching,
-        errors: state.vol.errors
+        errors: state.vol.errors,
+        food: state.donation.food,
+        isFetching: state.donation.isFetching,
+        errors: state.donation.errors
     }
 }
 
 export default connect(
     mapStateToProps,
-    { fetchVolunteer }
+    { fetchVolunteer, fetchFood }
 )(VolunteerProfile)
