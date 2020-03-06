d1d20f985f2310d0277416c75a64ee731ea77810
William Ryan <billy.ryan.rom@gmail.com>
2020-03-05 20:43:51 -0500

    Add inputHandler and submitHandler to EditProfile.js

--- replate/src/components/volunteer/EditProfile.js
+++ replate/src/components/volunteer/EditProfile.js
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 
 import { editVolunteer } from "../../actions"
 
 const EditVolunteer = props => {
-    return <div>Edit Here!</div>
+    const [editVol, setEditVol ] = useState({
+        username: '',
+        name: '',
+        phoneNumber: ''
+    })
+
+    const inputHandler = e => {
+        e.preventDefault();
+        setEditVol({ ...editVol, [e.target.name]: e.target.value })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        props.editVolunteer(editVol);
+    }
+
+    return (
+        <div></div>
+    )
 }
 
 export default EditVolunteer;
