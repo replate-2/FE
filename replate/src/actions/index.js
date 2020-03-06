f2abc10898c78cacf4412d574d52c6e3a7007178
William Ryan <billy.ryan.rom@gmail.com>
2020-03-06 04:15:51 -0500

    Add Food Post Request to index.js actions

--- replate/src/actions/index.js
+++ replate/src/actions/index.js
@@ -54,3 +54,21 @@ export const deleteBusiness = business => dispatch => {
             dispatch({ type: DELETE_FAIL, payload: err })
         })
 }
+
+export const POST_DATA = "POST_DATA"
+export const POST_SUCCESS = "POST_SUCCESS"
+export const POST_FAIL = "POST_FAIL"
+export const addFoodRequest = food => dispatch => {
+    dispatch({ type: POST_DATA })
+
+    axiosWithAuth()
+        .post(`api/food`, food)
+        .then(res => {
+            console.log(res.data);
+            dispatch({ type: POST_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({ type: POST_FAIL, payload: err })
+        })
+}
