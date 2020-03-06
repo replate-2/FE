49c7328df16169f1e882c78a00abcaed2423d2f8
William Ryan <billy.ryan.rom@gmail.com>
2020-03-06 04:25:09 -0500

    Create foodReducer in foodReducer.js/Add POST actions in foodReducer

--- replate/src/reducers/foodReducer.js
+++ replate/src/reducers/foodReducer.js
@@ -4,3 +4,37 @@ import {
     POST_FAIL
 } from "../actions/index"
 
+const initialState = {
+    food: [],
+    isPosting: false,
+    errors: ''
+}
+
+ export const foodReducer = (state = initialState, action) => {
+    switch(action.type){
+        case POST_DATA:
+            return {
+                ...state,
+                food: [
+                    ...state.food
+                ],
+                isPosting: true,
+                errors: ''
+            }
+        case POST_SUCCESS:
+            return {
+                ...state,
+                food: action.payload,
+                isPosting: false,
+                errors: ''
+            }
+        case POST_FAIL: 
+            return {
+                ...state,
+                isPosting: false,
+                errors: action.payload
+            }
+        default:
+            return state
+    }
+}
