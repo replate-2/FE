aac8dd362b8c13fd91474512235487ce667d19f3
William Ryan <billy.ryan.rom@gmail.com>
2020-03-06 04:44:02 -0500

    Add connect to PickupForm and mapStateToProps

--- replate/src/components/business/Pickup.js
+++ replate/src/components/business/Pickup.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
+import { connect } from "react-redux"
+
+import { addFoodRequest } from "../../actions/index"
 
 const PickupForm = props => {
     return <div></div>
 }
 
-export default PickupForm
+const mapStateToProps = state => {
+    return {
+        food: state.donation.food,
+        errors: state.donation.errors
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { addFoodRequest }
+)(PickupForm)
